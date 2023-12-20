7d84af60baf12b9cd71c99bb8e61ccad1c705572
taro1265 <taro1265h@gmail.com>
2023-12-21 05:53:28 +0900

    update backend

---
+++ backend/apps/time_manage/urls.py
@@ -0,0 +1,21 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import UserViewSet, SemesterInfoViewSet, TimeInfoViewSet, MusicInfoViewSet, PlayerInfoViewSet, ComposerInfoViewSet, ConductorInfoViewSet, OrchestraInfoViewSet, SemesterUserInfoViewSet, SemesterUserInfoPostViewSet, SemesterInfoPostViewSet
+
+router = DefaultRouter()
+router.register(r'users', UserViewSet)
+
+router.register(r'music', MusicInfoViewSet)
+router.register(r'player', PlayerInfoViewSet)
+router.register(r'semester', SemesterInfoViewSet, basename='semester')
+router.register(r'semesterpost', SemesterInfoPostViewSet, basename='semesterpost')
+router.register(r'time', TimeInfoViewSet)
+router.register(r'composer', ComposerInfoViewSet)
+router.register(r'conductor', ConductorInfoViewSet)
+router.register(r'orchestra', OrchestraInfoViewSet)
+router.register(r'semesteruser', SemesterUserInfoViewSet, basename='semesteruser')
+router.register(r'semesteruserpost', SemesterUserInfoPostViewSet, basename='semesteruserpost')
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
